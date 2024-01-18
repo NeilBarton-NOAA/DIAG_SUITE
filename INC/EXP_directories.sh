b1778ef9f1aa245f5cd0e15f7dd564067f24b259
NeilBarton-NOAA <neil.barton@noaa.gov>
2024-01-18 22:46:51 +0000

    simple edits

--- INC/EXP_directories.sh
+++ INC/EXP_directories.sh
@@ -6,5 +6,6 @@ case ${EXP} in
     'HR2' ) DIR_HPSS="/NCEPDEV/emc-climate/5year/role.ufscpara/{WCOSS2,HERA}/HR2";;
     'EP4' ) DIR_HPSS='/NCEPDEV/emc-ensemble/2year/Bing.Fu/ep4/ep4_f';;
     'EP4a' ) DIR_HPSS='/NCEPDEV/emc-ensemble/2year/Bing.Fu/ep4a';;
+    'GDAS' ) DIR_HPSS='/NCEPDEV/emc-ensemble/2year/Bing.Fu/ep4a';;
     * ) echo 'FATAL: case unknowned ' && exit 1
 esac
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # DIAG_SUITE
 diagnostic tools for ufs/gefs/gfs/sfs
+
+DEPENDENCIES:
+    cylc, python3 (matplotlib, xesmf, xarray, others)
+
+MODULES FOR DEPENDENCIES:
+    module use -a ~Neil.Barton/TOOLS/modulefiles
+    module load conda
+    module cylc
+    
+
