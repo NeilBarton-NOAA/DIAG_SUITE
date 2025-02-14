68a60bea45cd44a1a1ec14332d2ce1b69cd6c9dd
NeilBarton-NOAA <neil.barton@noaa.gov>
2025-02-14 20:41:18 +0000

    update EP5r1 experiment hpss locatoin

--- SCRIPTS/experiment_options.sh
+++ SCRIPTS/experiment_options.sh
@@ -21,24 +21,14 @@ export TOPDIR_FIGURES=${WORK_DIR}/${USER}/FIGURES
 # Location of based on experiment
 case ${EXP} in 
     'EP5r1')    
-        SRC_DIR='/NCEPDEV/emc-ensemble/2year/Bing.Fu/ep5r1' 
+        SRC_DIR='/NCEPDEV/emc-ensemble/2year/Bing.Fu/ep5r2_f' 
         WORKFLOW="GEFS"
-        #local_download_dir=${TOPDIR_OUTPUT}/${EXP}/${dtg:0:8}
-        #local_ice_dir=${local_download_dir}/ice 
-        #hpss_file=${SRC_DIR}/${dtg:0:4}/${dtg:0:6}/${dtg:0:8}/gefs.${dtg:0:8}_${dtg:8:10}.atmos.ice.tar 
-        #FL=35 # Forecast Length is 35 days
-        #CICE_SUFFIX="d"
+        RUN="gefs"
         ;;
     'EP6')    
         SRC_DIR='/NCEPDEV/emc-ensemble/5year/emc.ens/WCOSS2/EP6'
         WORKFLOW="GW"
         RUN="gefs"
-        #hpss_file=${SRC_DIR}/${dtg}/gefs_ice.tar
-        #local_download_dir=${TOPDIR_OUTPUT}/${EXP}
-        #local_ice_dir=${local_download_dir}/gefs.${dtg:0:8}/${dtg:8:10}/mem*/model/ice/history
-        #file_search="gefs.ice.t??z.*.f*.nc"
-        #FL=48 # Forecast Length is 48 days
-        #CICE_SUFFIX="h"
         ;;
      'DUMMY')
         echo "DUMMY EXP for scripts"
