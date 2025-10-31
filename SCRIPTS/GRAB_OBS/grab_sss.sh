2495fd71ab87a90450e1b4dfeceaf5d2f25e729c
Neil Barton <103681022+NeilBarton-NOAA@users.noreply.github.com>
2025-10-31 15:21:25 -0400

    Fix destination path in grab_sss.sh script

--- SCRIPTS/GRAB_OBS/grab_sss.sh
+++ SCRIPTS/GRAB_OBS/grab_sss.sh
@@ -3,6 +3,6 @@ set -xu
 DTG=${1:0:8} 
 SCRIPT_DIR=${SCRIPT_DIR:-$(dirname "$0")/../}
 source ${SCRIPT_DIR}/experiment_options.sh DUMMY ${DTG}00
-DES=${TOPDIR_OBS}/sss/ostia
+DES=${TOPDIR_OBS}/sss
 
 ${SCRIPT_DIR}/GRAB_OBS/cm.py sss ${DTG} ${DES} 48
